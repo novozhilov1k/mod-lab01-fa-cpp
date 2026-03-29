6d8d2bc44de71f0f332bd1005327565c272e8cdd
novozhilov1k <novozhilovkirill793@gmail.com>
2026-03-29 23:27:50 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,11 @@
 
 int main() {
     int a = faStr1("asd 12 sda");
-    std::coutcout <<a;
+    std::cout <<a;
 
     int b = faStr2("Adas da Ada");
-    std::coutcout <<b;
+    std::cout <<b;
 
     int c = faStr3("dasd dwadawda dasd a");
-    std::coutcout <<c;
+    std::cout<<c;
 }

94fa1db8535f12d69ca3fc0cdd1fc0c46059fda3
novozhilov1k <novozhilovkirill793@gmail.com>
2026-03-29 23:38:39 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,13 @@
 // Copyright 2022 UNN-IASR
-#include "fun.h"
 #include <iostream>
-
+#include "fun.h"
 int main() {
     int a = faStr1("asd 12 sda");
-    std::cout <<a;
+    std::cout << a;
 
     int b = faStr2("Adas da Ada");
-    std::cout <<b;
+    std::cout << b;
 
     int c = faStr3("dasd dwadawda dasd a");
-    std::cout<<c;
+    std::cout << c;
 }
